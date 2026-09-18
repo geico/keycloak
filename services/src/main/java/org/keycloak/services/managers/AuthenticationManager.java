8ffbe809e57c3e8e9e94570b341ef8ccad700ff0
Hockenba <github@opalpine.com>
2026-09-18 16:43:45 -0400

    plaid review changes

--- services/src/main/java/org/keycloak/services/managers/AuthenticationManager.java
+++ services/src/main/java/org/keycloak/services/managers/AuthenticationManager.java
@@ -1770,8 +1770,7 @@ public class AuthenticationManager {
                         user,
                         session.getContext().getConnection(),
                         session.getContext().getHttpRequest().getUri(),
-                        Set.copyOf(AuthenticatorUtil.getAuthnCredentials(authSession)),
-                        authSession.getAuthNote(AbstractUsernameFormAuthenticator.ATTEMPTED_USERNAME)
+                        Set.copyOf(AuthenticatorUtil.getAuthnCredentials(authSession))
                 );
             }
         }
